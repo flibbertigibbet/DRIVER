1d6efd0b0164cf439e941dd3822903dfe8283739
Kathryn Killebrew <banderkat@gmail.com>
2016-02-21 00:22:24 -0500

    Use python3, which allows subprocess timeout argument

--- gradle/sub.py
+++ gradle/sub.py
@@ -3,6 +3,7 @@
 import json
 import os
 import sys
+import time
 
 import redis
 import subprocess
@@ -11,7 +12,7 @@ import subprocess
 DRIVER_REDIS_HOST = os.getenv('DRIVER_REDIS_HOST', '192.168.12.101')
 DRIVER_REDIS_PORT = os.getenv('DRIVER_REDIS_PORT', 6379)
 DRIVER_REDIS_JAR_DB = os.getenv('DRIVER_REDIS_JAR_DB', 3)
-JAR_TTL_SECONDS = os.getenv('DRIVER_JAR_TTL_SECONDS', 60 * 60 * 24 * 30)
+TIMEOUT_SECONDS = 60
 
 
 print('\nSetting up redis subscription for gradle task')
@@ -33,15 +34,17 @@ for message in pub.listen():
     if message['type'] == 'message':
         try:
             data = message['data']
-            print('got jar build message: {message}'.format(message=data))
-            data = json.loads(data)
+            data = json.loads(data.decode('utf-8'))
             uuid = data['uuid']
             schema = json.dumps(data['schema'])
-            subprocess.check_call(['/bin/bash', 'run.sh', uuid, schema, JAR_TTL_SECONDS])
+            subprocess.check_call(['/bin/bash', 'run.sh', uuid, schema],
+                timeout=TIMEOUT_SECONDS)
         except (ValueError, KeyError) as ex:
             print('malformed message received: {message}'.format(message=message))
         except subprocess.CalledProcessError as ex:
             print('jar build process failed with: {ex}'.format(ex=ex))
+        except subprocess.TimeoutExpired as ex:
+            print('jar build process timed out: {ex}'.format(ex=ex))
 
 print('Redis subscription loop exited! Was there an error with the connection?')
 pub.close()
